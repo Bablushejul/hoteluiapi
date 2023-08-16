2770ff8c1591f8db3a072f6274646ae2f86a73bf
Bablushejul <shejulbablu@gmail.com>
2023-08-16 13:02:01 +0530

    FINAL

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1>i am</h1>
-      <h2>i am</h2>
-      <h3>i am</h3>
-      <h4>i am</h4>
-      <h5>i am</h5>
-      <h6>i am</h6>
-    
+      
+    <h1>Welcome</h1>
     </>
   );
 }
